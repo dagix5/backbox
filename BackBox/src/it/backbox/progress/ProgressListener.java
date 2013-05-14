f14be9879ac69882c3dbb236120035b543e4a657
dagix5 <dagix5@gmail.com>
2013-05-14 18:16:36 +0000

    speed/progress configuration per stream ID

--- BackBox/src/it/backbox/progress/ProgressListener.java
+++ BackBox/src/it/backbox/progress/ProgressListener.java
@@ -2,6 +2,14 @@ package it.backbox.progress;
 
 public interface ProgressListener {
 	
-	void update(long bytes);
+	/**
+	 * Method called when an update (read/write) is performed in a stream
+	 * 
+	 * @param id
+	 *            ID of the stream
+	 * @param bytes
+	 *            Bytes read or written
+	 */
+	void update(String id, long bytes);
 
 }
