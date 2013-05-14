d5ae373cc899539650f94eb00c6fdd08541edbec
dagix5@gmail.com <dagix5@gmail.com@60a4aeb7-9827-59fe-9812-c1f8a06d2ea5>
2013-05-14 18:13:19 +0000

    fix retry upload on 409 conflict

--- trunk/BackBox/src/it/backbox/client/rest/bean/BoxContextInfo.java
+++ trunk/BackBox/src/it/backbox/client/rest/bean/BoxContextInfo.java
@@ -1,5 +1,11 @@
 package it.backbox.client.rest.bean;
 
+import java.util.List;
+
+import com.google.api.client.util.Key;
+
 public class BoxContextInfo {
 
+	@Key
+	public List<BoxFile> conflicts;
 }
