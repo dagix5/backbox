cc63d1aa83982e0f0a72baa82ac91b93ec3223e0
dagix5@gmail.com <dagix5@gmail.com@60a4aeb7-9827-59fe-9812-c1f8a06d2ea5>
2013-05-14 19:12:04 +0000

    log configuration in preferences dialog

--- trunk/BackBoxGui/src/it/backbox/gui/PreferencesDialog.java
+++ trunk/BackBoxGui/src/it/backbox/gui/PreferencesDialog.java
@@ -6,18 +6,23 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
 import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
 
 import net.miginfocom.swing.MigLayout;
 
 import org.apache.commons.configuration.ConfigurationException;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerNumberModel;
 
 public class PreferencesDialog extends JDialog {
 	private static final long serialVersionUID = 1L;
@@ -51,7 +56,7 @@ public class PreferencesDialog extends JDialog {
 
 		JPanel panel = new JPanel();
 		getContentPane().add(panel, BorderLayout.CENTER);
-		panel.setLayout(new MigLayout("", "[141.00px][49.00px,grow][250.00]", "[20px][][][][][]"));
+		panel.setLayout(new MigLayout("", "[141.00px][49.00px,grow][250.00]", "[20px][][][][][][]"));
 
 		JLabel lblFolderToBackup = new JLabel("Folder to backup");
 		panel.add(lblFolderToBackup, "cell 0 0,alignx trailing,aligny center");
@@ -62,7 +67,7 @@ public class PreferencesDialog extends JDialog {
 		panel.add(backupFolder, "cell 1 0 2 1,growx,aligny top");
 		
 		JLabel lblDefaultUploadSpeed = new JLabel("Default upload speed");
-		panel.add(lblDefaultUploadSpeed, "cell 0 1");
+		panel.add(lblDefaultUploadSpeed, "cell 0 1,alignx right");
 		
 		defaultUploadSpeed = new JSpinner();
 		defaultUploadSpeed.setModel(new SpinnerNumberModel(new Integer(0), new Integer(0), null, new Integer(1)));
@@ -70,6 +75,22 @@ public class PreferencesDialog extends JDialog {
 		
 		JLabel lblKbsset = new JLabel("KB\\s (set 0 for unlmited)");
 		panel.add(lblKbsset, "cell 2 1");
+		
+		JLabel lblLogLevel = new JLabel("Log level");
+		panel.add(lblLogLevel, "cell 0 2,alignx trailing");
+		
+		JComboBox<Level> comboBox = new JComboBox<Level>();
+		comboBox.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent arg0) {
+				Logger.getLogger("it.backbox").setLevel((Level) arg0.getItem());
+			}
+		});
+		comboBox.addItem(Level.ALL);
+		comboBox.addItem(Level.INFO);
+		comboBox.addItem(Level.SEVERE);
+		comboBox.addItem(Level.OFF);
+		comboBox.setSelectedIndex(2);
+		panel.add(comboBox, "cell 1 2 2 1,growx");
 	}
 
 }
