2e74a9c4ae37bf4fd028ba059c10958c1b108967
dagix5@gmail.com <dagix5@gmail.com@60a4aeb7-9827-59fe-9812-c1f8a06d2ea5>
2013-05-14 18:16:36 +0000

    speed/progress configuration per stream ID

--- trunk/BackBox/src/it/backbox/progress/stream/InputStreamCounter.java
+++ trunk/BackBox/src/it/backbox/progress/stream/InputStreamCounter.java
@@ -11,12 +11,25 @@ public class InputStreamCounter extends FilterInputStream {
 	private ProgressManager manager;
 	private String id;
 
+	/**
+	 * Constructor
+	 * 
+	 * @param in
+	 *            Original InputStream
+	 * @param id
+	 *            Stream ID
+	 */
 	public InputStreamCounter(final InputStream in, String id) {
 		super(in);
 		this.id = id;
 		manager = ProgressManager.getInstance();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see java.io.FilterInputStream#read(byte[], int, int)
+	 */
+	@Override
 	public int read(byte[] b, int off, int len) throws IOException {
 		int r = 0;
 		int speed = manager.getSpeed(id);
@@ -26,15 +39,19 @@ public class InputStreamCounter extends FilterInputStream {
 				manager.consume(id, speed);
 				r += super.read(b, (off + plen), speed);
 				plen += speed;
-				if (manager.getListener(id) != null) manager.getListener(id).update(speed);
+				if (manager.getListener(id) != null) manager.getListener(id).update(id, speed);
 			}
 		}
 		manager.consume(id, len - plen);
 		r += super.read(b, (off + plen), (len - plen));
-		if (manager.getListener(id) != null) manager.getListener(id).update(len - plen);
+		if (manager.getListener(id) != null) manager.getListener(id).update(id, len - plen);
 		return r;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see java.io.FilterInputStream#read(byte[])
+	 */
 	@Override
 	public int read(byte[] b) throws IOException {
 		return read(b, 0, b.length);
--- trunk/BackBox/src/it/backbox/progress/stream/OutputStreamCounter.java
+++ trunk/BackBox/src/it/backbox/progress/stream/OutputStreamCounter.java
@@ -11,12 +11,25 @@ public class OutputStreamCounter extends FilterOutputStream {
 	private ProgressManager manager;
 	private String id;
     
+	/**
+	 * Constructor
+	 * 
+	 * @param in
+	 *            Original OutputStream
+	 * @param id
+	 *            Stream ID
+	 */
     public OutputStreamCounter(final OutputStream out, String id) {
         super(out);
         this.id = id;
 		manager = ProgressManager.getInstance();
     }
 
+    /*
+     * (non-Javadoc)
+     * @see java.io.FilterOutputStream#write(byte[], int, int)
+     */
+    @Override
     public void write(byte[] b, int off, int len) throws IOException {
     	int speed = manager.getSpeed(id);
     	int plen = 0;
@@ -25,16 +38,21 @@ public class OutputStreamCounter extends FilterOutputStream {
 	    		manager.consume(id, speed);
 	    		super.write(b, (off + plen), speed);
 	    		plen += speed;
-	    		if (manager.getListener(id) != null) manager.getListener(id).update(speed);
+	    		if (manager.getListener(id) != null) manager.getListener(id).update(id, speed);
 	    	}
     	}
     	super.write(b, (off + plen), (len - plen));
-        if (manager.getListener(id) != null) manager.getListener(id).update(len - plen);
+        if (manager.getListener(id) != null) manager.getListener(id).update(id, len - plen);
     }
 
+    /*
+     * (non-Javadoc)
+     * @see java.io.FilterOutputStream#write(int)
+     */
+    @Override
     public void write(int b) throws IOException {
     	manager.consume(id, 1);
         super.write(b);
-        if (manager.getListener(id) != null) manager.getListener(id).update(1);
+        if (manager.getListener(id) != null) manager.getListener(id).update(id, 1);
     }
 }
