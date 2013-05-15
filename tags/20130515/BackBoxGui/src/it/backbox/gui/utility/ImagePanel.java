91ff7cc391d6c89b188d19db09932a63c68d8aa7
dagix5@gmail.com <dagix5@gmail.com@60a4aeb7-9827-59fe-9812-c1f8a06d2ea5>
2013-05-15 20:09:41 +0000

    first release gui

---
+++ tags/20130515/BackBoxGui/src/it/backbox/gui/utility/ImagePanel.java
@@ -0,0 +1,23 @@
+package it.backbox.gui.utility;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.JPanel;
+
+public class ImagePanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	private final Image image;
+
+    public ImagePanel(Image image) {
+        super();
+        this.image = image;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(this.image, 0, 0, getWidth(), getHeight(), this);
+    }
+}
