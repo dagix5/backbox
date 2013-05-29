32cc37f2661d4fb2a0fef836299e628268a9dc2a
dagix5@gmail.com <dagix5@gmail.com@60a4aeb7-9827-59fe-9812-c1f8a06d2ea5>
2013-05-29 18:32:44 +0000

    fixed quotes bug in filename, improved exception management

--- trunk/BackBox/src/it/backbox/exception/BackBoxException.java
+++ trunk/BackBox/src/it/backbox/exception/BackBoxException.java
@@ -1,9 +1,19 @@
 package it.backbox.exception;
 
+import java.sql.SQLException;
+
 public class BackBoxException extends Exception {
 	
 	public BackBoxException(String string) {
 		super(string);
+	}
+	
+	public BackBoxException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+	public BackBoxException(SQLException sqle, String query) {
+		super("Database: error executing query: " + query, sqle);
 	}
 
 	private static final long serialVersionUID = 1L;
