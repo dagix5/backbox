f06b6c5cea92a79b07d8d701a77efd9c531d0621
dagix5@gmail.com <dagix5@gmail.com@60a4aeb7-9827-59fe-9812-c1f8a06d2ea5>
2013-07-09 15:02:16 +0000

    fixed logger configuration and loading dialog behavior

--- trunk/BackBoxGui/src/it/backbox/gui/PreferencesDialog.java
+++ trunk/BackBoxGui/src/it/backbox/gui/PreferencesDialog.java
@@ -20,7 +20,6 @@ import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JSeparator;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
@@ -41,6 +40,7 @@ public class PreferencesDialog extends JDialog {
 	private JTextField txtProxyPort;
 	private JTextField txtProxyAddress;
 	private JCheckBox chckbxProxy;
+	private JSpinner logSize;
 	
 	private Level newLevel = null;
 
@@ -52,7 +52,7 @@ public class PreferencesDialog extends JDialog {
 		setModalityType(ModalityType.APPLICATION_MODAL);
 		
 		setTitle("Preferences");
-		setBounds(100, 100, 450, 220);
+		setBounds(100, 100, 450, 241);
 		getContentPane().setLayout(new BorderLayout());
 		
 		JPanel buttonPane = new JPanel();
@@ -69,7 +69,7 @@ public class PreferencesDialog extends JDialog {
 
 		final JPanel panel = new JPanel();
 		getContentPane().add(panel, BorderLayout.CENTER);
-		panel.setLayout(new MigLayout("", "[141.00px][49.00px,grow][250.00]", "[][][][][][][][][][][][][]"));
+		panel.setLayout(new MigLayout("", "[120px:120px][90px:90px][150px:n,grow]", "[][][][][][]"));
 		
 		JLabel lblDefaultUploadSpeed = new JLabel("Default upload speed");
 		panel.add(lblDefaultUploadSpeed, "cell 0 0,alignx right");
@@ -98,23 +98,29 @@ public class PreferencesDialog extends JDialog {
 		
 		panel.add(comboBox, "cell 1 1 2 1,growx");
 		
-		JSeparator separator = new JSeparator();
-		panel.add(separator, "cell 0 2 3 1");
+		JLabel lblLogSize = new JLabel("Log size");
+		panel.add(lblLogSize, "cell 0 2,alignx right");
+		
+		logSize = new JSpinner();
+		panel.add(logSize, "cell 1 2,grow");
+		
+		JLabel lblKb = new JLabel("KB");
+		panel.add(lblKb, "cell 2 2,alignx left,growy");
 		
 		JLabel lblProxyAddress = new JLabel("Proxy Address");
-		panel.add(lblProxyAddress, "cell 0 5,alignx trailing");
+		panel.add(lblProxyAddress, "cell 0 4,alignx trailing");
 		
 		txtProxyAddress = new JTextField();
 		txtProxyAddress.setEnabled(false);
-		panel.add(txtProxyAddress, "cell 1 5 2 1,growx");
+		panel.add(txtProxyAddress, "cell 1 4 2 1,growx");
 		txtProxyAddress.setColumns(10);
 		
 		JLabel lblProxyPort = new JLabel("Proxy Port");
-		panel.add(lblProxyPort, "cell 0 6,alignx trailing");
+		panel.add(lblProxyPort, "cell 0 5,alignx trailing");
 		
 		txtProxyPort = new JTextField();
 		txtProxyPort.setEnabled(false);
-		panel.add(txtProxyPort, "cell 1 6 2 1,growx");
+		panel.add(txtProxyPort, "cell 1 5 2 1,growx");
 		Document doc = txtProxyPort.getDocument();
 		if (doc instanceof AbstractDocument) {
 			AbstractDocument abDoc = (AbstractDocument) doc;
@@ -128,7 +134,7 @@ public class PreferencesDialog extends JDialog {
 				txtProxyPort.setEnabled(chckbxProxy.isSelected());
 			}
 		});
-		panel.add(chckbxProxy, "cell 0 4,alignx right");
+		panel.add(chckbxProxy, "cell 0 3,alignx right");
 		
 		JButton okButton = new JButton("OK");
 		okButton.addActionListener(new ActionListener() {
@@ -141,13 +147,15 @@ public class PreferencesDialog extends JDialog {
 							main.setPreferences(((Integer) defaultUploadSpeed.getValue()) * 1024);
 							if (newLevel != null) {
 								Logger.getLogger("it.backbox").setLevel(newLevel);
-								main.helper.getConfiguration().setLogLevel(newLevel);
+								main.helper.getConfiguration().setLogLevel(newLevel.getName());
 							}
 							
 							ProxyConfiguration pc = new ProxyConfiguration(chckbxProxy.isSelected(), txtProxyAddress.getText(), (txtProxyPort.getText().isEmpty() ? 0 : Integer.parseInt(txtProxyPort.getText())));
 							main.helper.setProxyConfiguration(pc);
 							
 							main.helper.getConfiguration().setProxyConfiguration(pc);
+							//after restart, the new size will be read
+							main.helper.getConfiguration().setLogSize((int) logSize.getValue() * 1024);
 							main.helper.saveConfiguration();
 							
 							setVisible(false);
@@ -171,8 +179,9 @@ public class PreferencesDialog extends JDialog {
 		getRootPane().setDefaultButton(okButton);
 	}
 
-	public void load(int uploadSpeed, ProxyConfiguration pc, boolean proxyChcbxEnabled, Level logLevel) {
+	public void load(int uploadSpeed, ProxyConfiguration pc, boolean proxyChcbxEnabled, Level logLevel, int logSize) {
 		defaultUploadSpeed.setModel(new SpinnerNumberModel(uploadSpeed / 1024, new Integer(0), null, new Integer(1)));
+		this.logSize.setModel(new SpinnerNumberModel((logSize > 1024) ? logSize / 1024 : 1, new Integer(1), null, new Integer(1)));
 		chckbxProxy.setEnabled(proxyChcbxEnabled);
 		if (pc != null) {
 			chckbxProxy.setSelected(pc.isEnabled());
@@ -184,18 +193,20 @@ public class PreferencesDialog extends JDialog {
 			txtProxyPort.setText("");
 		}
 		
-		if (logLevel.equals(Level.ALL))
-			comboBox.setSelectedIndex(0);
-		else if (logLevel.equals(Level.FINE))
-			comboBox.setSelectedIndex(1);
-		else if (logLevel.equals(Level.INFO))
-			comboBox.setSelectedIndex(2);
-		else if (logLevel.equals(Level.WARNING))
-			comboBox.setSelectedIndex(3);
-		else if (logLevel.equals(Level.SEVERE))
-			comboBox.setSelectedIndex(4);
-		else if (logLevel.equals(Level.OFF))
-			comboBox.setSelectedIndex(5);
+		if (logLevel != null) {
+			if (logLevel.equals(Level.ALL))
+				comboBox.setSelectedIndex(0);
+			else if (logLevel.equals(Level.FINE))
+				comboBox.setSelectedIndex(1);
+			else if (logLevel.equals(Level.INFO))
+				comboBox.setSelectedIndex(2);
+			else if (logLevel.equals(Level.WARNING))
+				comboBox.setSelectedIndex(3);
+			else if (logLevel.equals(Level.SEVERE))
+				comboBox.setSelectedIndex(4);
+			else if (logLevel.equals(Level.OFF))
+				comboBox.setSelectedIndex(5);
+		}
 	}
 	
 	class DocumentInputFilter extends DocumentFilter {
